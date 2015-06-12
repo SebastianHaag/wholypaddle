d75928672ab71b5a1fb9c62cad46b3e89d647d66
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-06-12 11:47:01 +0200

    fixed

--- js/app.js
+++ js/app.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
    $(window).load(function() {
       $("#preload").fadeOut(600);
    });
+   $(document).foundation({
+      reveal: {
+         animation: 'fade',
+      }
+   });
+   $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+      $('html').addClass('modal-open');
+   });
+
+   $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+      $('html').removeClass('modal-open');
+   });
 });
